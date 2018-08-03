678763ae3e985d1e43281dadef0f2d7e3ed5725e
Olamide Laleye <laleyeolamide@yahoo.com>
2018-08-04 00:00:08 +0100

    Bind data to views

--- app/src/main/java/com/olamide/latestmovies/activity/MainActivity.java
+++ app/src/main/java/com/olamide/latestmovies/activity/MainActivity.java
@@ -2,15 +2,21 @@ package com.olamide.latestmovies.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.olamide.latestmovies.Config;
 import com.olamide.latestmovies.R;
+import com.olamide.latestmovies.adapter.MovieAdapter;
+import com.olamide.latestmovies.bean.Movie;
 import com.olamide.latestmovies.bean.TMDBMovieResponse;
 import com.olamide.latestmovies.network.TMDBMoviesService;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,26 +28,30 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    @BindView(R.id.test_tv)
-    TextView mTestTv;
+    @BindView(R.id.rv_movies)
+    RecyclerView mRecyclerView;
+
+    private List<Movie> movieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ButterKnife.bind(this);
+
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
+
 
 
         TMDBMoviesService.getMoviesByTopRated(Config.TMDB_API_KEY, new Callback<TMDBMovieResponse>() {
             @Override
             public void onResponse(Call<TMDBMovieResponse> call, Response<TMDBMovieResponse> response) {
                 Log.e(TAG, call.request().toString());
-                //movies = response.body().getResults();
-                //recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
 
-                TMDBMovieResponse  movieResponse = response.body();
-                Log.e(TAG, ReflectionToStringBuilder.toString(movieResponse));
-                mTestTv.setText(ReflectionToStringBuilder.toString(movieResponse));
+                Log.e(TAG, ReflectionToStringBuilder.toString(response));
+                movieList = response.body().getResults();
+                mRecyclerView.setAdapter(new MovieAdapter(movieList,  getApplicationContext()));
 
             }
 
@@ -54,5 +64,38 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
     }
+
+
+
+
+    public void popuplatemovies(){
+
+        TMDBMoviesService.getMoviesByTopRated(Config.TMDB_API_KEY, new Callback<TMDBMovieResponse>() {
+            @Override
+            public void onResponse(Call<TMDBMovieResponse> call, Response<TMDBMovieResponse> response) {
+                Log.e(TAG, call.request().toString());
+                //movies = response.body().getResults();
+                //recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+
+                TMDBMovieResponse  movieResponse = response.body();
+                Log.e(TAG, ReflectionToStringBuilder.toString(movieResponse));
+
+
+            }
+
+            @Override
+            public void onFailure(Call<TMDBMovieResponse> call, Throwable t) {
+
+                Log.e(TAG, "ERROR  " +t.toString());
+            }
+        });
+
+
+    }
+
+
+
 }
---
+++ app/src/main/java/com/olamide/latestmovies/adapter/MovieAdapter.java
@@ -0,0 +1,83 @@
+package com.olamide.latestmovies.adapter;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.olamide.latestmovies.Config;
+import com.olamide.latestmovies.R;
+import com.olamide.latestmovies.bean.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapterViewHolder> {
+
+
+
+
+    private List<Movie> movieList;
+    private Context context;
+
+    public MovieAdapter(List<Movie> movies, Context context) {
+        this.movieList = movies;
+        this.context = context;
+    }
+
+
+
+    public static class MovieAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.tv_movie_title)
+        TextView title;
+        @BindView(R.id.iv_movie)
+        ImageView movieImage;
+
+        public MovieAdapterViewHolder(View v) {
+            super(v);
+            ButterKnife.bind(this, v);
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public MovieAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.movie_item, viewGroup, false);
+
+        return new MovieAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieAdapterViewHolder holder, int position) {
+
+        String image_url = Config.TMDB_IMAGE_URL_PATH + movieList.get(position).getPosterPath();
+        Picasso.with(context)
+                .load(image_url)
+                .placeholder(android.R.drawable.sym_def_app_icon)
+                .error(android.R.drawable.sym_def_app_icon)
+                .into(holder.movieImage);
+        holder.title.setText(movieList.get(position).getTitle());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(movieList != null){
+            return movieList.size();
+        }else {
+            return 0;
+        }
+    }
+}
