b692351dab99eb1f499afdb37c0ce4e0c90b8317
Olamide Laleye <laleyeolamide@yahoo.com>
2018-08-15 23:45:07 +0100

    Initialize room

--- app/src/main/java/com/olamide/latestmovies/activity/MovieDetails.java
+++ app/src/main/java/com/olamide/latestmovies/activity/MovieDetails.java
@@ -2,8 +2,10 @@ package com.olamide.latestmovies.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.olamide.latestmovies.Config;
 import com.olamide.latestmovies.R;
@@ -12,6 +14,7 @@ import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MovieDetails extends AppCompatActivity {
 
@@ -30,7 +33,13 @@ public class MovieDetails extends AppCompatActivity {
     @BindView(R.id.iv_movie_poster)
     ImageView ivPoster;
 
+    @BindView(R.id.bt_favourite)
+    Button btFavourite;
+
     private Movie movie;
+
+    String favouriteText;
+    boolean favouriteMovie = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +48,18 @@ public class MovieDetails extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+
+
+        if(favouriteMovie){
+            favouriteText = this.getResources().getString(R.string.remove_favourite);
+        }else {
+            favouriteText = this.getResources().getString(R.string.add_favourite);
+        }
+
         movie =  getIntent().getParcelableExtra(MainActivity.VIEW_MOVIE_OBJECT);
         populateMovie();
+
+
 
     }
 
@@ -50,6 +69,7 @@ public class MovieDetails extends AppCompatActivity {
         tvDate.setText(movie.getReleaseDate().substring(0,4));
         tvRatings.setText(Double.toString(movie.getVoteAverage()) + "/10");
         tvSynopsis.setText(movie.getOverview());
+        btFavourite.setText(favouriteText);
 
         String image_url = Config.TMDB_IMAGE_URL_PATH +movie.getPosterPath();
         Picasso.with(this)
@@ -58,6 +78,17 @@ public class MovieDetails extends AppCompatActivity {
                 .error(R.drawable.errorr)
                 .into(ivPoster);
 
+
+    }
+
+    @OnClick(R.id.bt_favourite)
+    public void setFavourite(){
+
+        if(favouriteMovie){
+            Toast.makeText(MovieDetails.this, "Successfully removed this movie from favourite  " ,Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(MovieDetails.this, "Successfully added this movie as favourite  " ,Toast.LENGTH_LONG).show();
+        }
 
     }
 }
